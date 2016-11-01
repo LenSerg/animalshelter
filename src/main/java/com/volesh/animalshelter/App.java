6b024d5776b7723491641dd76fa57507fd9da2b8
Len Sergey <len.sergey.a@gmail.com>
2016-11-01 15:42:11 +0700

    First entity commit

--- src/main/java/com/volesh/animalshelter/App.java
+++ src/main/java/com/volesh/animalshelter/App.java
@@ -1,9 +1,27 @@
 package com.volesh.animalshelter;
 
-public class App 
+import com.volesh.animalshelter.entity.Animal;
+import com.volesh.animalshelter.utils.HibernateUtil;
+import org.hibernate.Session;
+
+import java.util.List;
+
+public class App
 {
     public static void main( String[] args )
     {
+        App app = new App();
+        List<Animal> animals = app.listAnimal();
+        for (Animal animal : animals) {
+            System.out.println(animal.getName());
+        }
+    }
 
+    public List<Animal> listAnimal() {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        List<Animal> result = session.createQuery("from animal order by name").list();
+        session.getTransaction().commit();
+        return result;
     }
 }
---
+++ src/main/java/com/volesh/animalshelter/entity/Animal.java
@@ -0,0 +1,96 @@
+package com.volesh.animalshelter.entity;
+
+import java.util.Date;
+
+public class Animal {
+    private Long id;
+    private String name;
+    private String type;
+    private String breed;
+    private int age;
+    private String color;
+    private int sex;
+    private String specialSigns;
+    private String status;
+    private Date registrationDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public String getSpecialSigns() {
+        return specialSigns;
+    }
+
+    public void setSpecialSigns(String specialSigns) {
+        this.specialSigns = specialSigns;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
